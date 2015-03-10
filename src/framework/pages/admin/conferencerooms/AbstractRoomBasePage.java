fc9aba4bfb977fc583ffc492e3db79be69dd323f
YesicaAcha <yesica.7887@gmal.com>
2015-03-09 20:54:17 -0400

    AbstractRoomBasePage with conference rooms menu, save and cancel button

---
+++ src/framework/pages/admin/conferencerooms/AbstractRoomBasePage.java
@@ -0,0 +1,68 @@
+package framework.pages.admin.conferencerooms;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import framework.selenium.SeleniumDriverManager;
+
+/**
+ * This class represent the Menu for conference rooms, save and cancel button
+ * @author Yesica Acha
+ *
+ */
+public abstract class AbstractRoomBasePage {
+	protected WebDriver driver;	
+	protected WebDriverWait wait;
+
+	@FindBy(linkText="Room Info") 
+	WebElement roomInfoLink;
+
+	@FindBy(linkText="Resource Associations") 
+	WebElement resourceAssociationsLink;
+
+	@FindBy(linkText="Out of Order Planning") 
+	WebElement outOfOrderPlanningLink;
+
+	@FindBy(xpath="//button[@ng-click='save()']") 
+	WebElement saveBtn;
+
+	@FindBy(xpath="//button[@ng-click='cancel()']") 
+	WebElement cancelBtn;
+
+	public AbstractRoomBasePage() {
+		driver = SeleniumDriverManager.getManager().getDriver();
+		wait = SeleniumDriverManager.getManager().getWait();
+		PageFactory.initElements(driver, this);
+	}
+
+	public RoomInfoPage clickRoomInfoLink(){
+		roomInfoLink.click();
+		return new RoomInfoPage();
+	}
+
+	public CRResourceAssociationsPage clickResourceAssociationsLink(){
+		resourceAssociationsLink.click();
+		return new CRResourceAssociationsPage();
+	}
+
+	public OutOfOrderPlanningPage clickOutOfOrderPlanningLink(){
+		outOfOrderPlanningLink.click();
+		return new OutOfOrderPlanningPage();
+	}
+
+	public ConferenceRoomPage clickSaveBtn(){
+		saveBtn.click();
+		return new ConferenceRoomPage();
+	}
+
+	public ConferenceRoomPage clickCancelBtn(){
+		cancelBtn.click();
+		return new ConferenceRoomPage();
+	}
+
+
+
+}
