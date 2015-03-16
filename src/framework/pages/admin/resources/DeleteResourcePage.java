f7804bc94d9ecf98450ef312af4cc6b17e38ea56
Marco Llano <llano.marco@gmail.com>
2015-03-16 17:44:24 -0400

    Ruben comments fixed

--- src/framework/pages/admin/resources/DeleteResourcePage.java
+++ src/framework/pages/admin/resources/DeleteResourcePage.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import framework.common.UIMethods;
 import framework.selenium.SeleniumDriverManager;
 
 /**
@@ -12,8 +11,6 @@ import framework.selenium.SeleniumDriverManager;
  *
  */
 public class DeleteResourcePage extends AbstractResourceBasePage {
-	UIMethods uiMethod = new UIMethods();
-
 	@FindBy(css = "button.info")
 	WebElement confirmRemoveBtn;
 
