47ee1fe0ae59569c0f79cf3028d8161728ca64d2
YesicaAcha <yesica.7887@gmal.com>
2015-03-19 13:27:39 -0400

    getAdminHome method

--- src/framework/pages/admin/HomeAdminPage.java
+++ src/framework/pages/admin/HomeAdminPage.java
@@ -2,19 +2,28 @@ package framework.pages.admin;
 
 import static framework.common.AppConfigConstants.URL_ADMIN_HOME;
 
+import org.openqa.selenium.By;
+
+import framework.common.UIMethods;
+
 /**
  * 
  * @author Ruben Blanco
  *
  */
 public class HomeAdminPage extends AbstractMainMenu {
+	UIMethods uiMethods = new UIMethods();
 	
 	/**
 	 * [YA]This method returns to HomeAdminPage from Tablet
 	 * @return
 	 */
-	public HomeAdminPage getAdminHome(){
+	public HomeAdminPage getAdminHome() {
 		driver.get(URL_ADMIN_HOME);
+		By signInLocator = By.xpath("//button[@ng-click='signIn()']");
+		if(uiMethods.isElementPresent(signInLocator)) {
+			driver.findElement(signInLocator).click();
+		}
 		return this;
 	}
 }
