ac4af2220a4affe6c9c95b3305e36ff792ec76db
jcgr123 <jcgr123@hotmail.com>
2015-03-24 14:41:01 -0400

    changes in classes

--- src/framework/pages/admin/conferencerooms/RoomBaseAbstractPage.java
+++ src/framework/pages/admin/conferencerooms/RoomBaseAbstractPage.java
@@ -92,7 +92,7 @@ public abstract class RoomBaseAbstractPage {
 	 * @param errorMessage
 	 * @return
 	 */
-	public Object clickSaveWithErrorBtn(){
+	public RoomBaseAbstractPage clickSaveWithErrorBtn(){
 		saveBtn.click();
 		wait.until(ExpectedConditions.visibilityOf(errorMessageLbl));
 		return this;
