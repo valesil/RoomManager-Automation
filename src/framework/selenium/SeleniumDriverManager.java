93c947a3fc3027bc431327ff8bda4407523182a8
Eliana Navia <Eliana.Navia@fundacion-jala.org>
2015-03-25 14:16:05 -0400

    Logs

--- src/framework/selenium/SeleniumDriverManager.java
+++ src/framework/selenium/SeleniumDriverManager.java
@@ -9,12 +9,13 @@ import static framework.common.AppConfigConstants.WEBDRIVERWAIT_SLEEP;
 
 import java.util.concurrent.TimeUnit;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.log4testng.Logger;
 
 /**
  * Manages the web browser
@@ -24,8 +25,10 @@ public class SeleniumDriverManager {
 	private static SeleniumDriverManager manager = null;
 	private WebDriver driver;
 	private WebDriverWait wait;
-
+	Logger log = Logger.getLogger(getClass());
+	
 	protected SeleniumDriverManager() {
+		PropertyConfigurator.configure("log4j.properties");
 		initializeDriver();
 	}
 
@@ -33,16 +36,22 @@ public class SeleniumDriverManager {
 	 * Select a browser
 	 */
 	private void initializeDriver() {
+		
 		if (BROWSER.equalsIgnoreCase("firefox")) {
+			log.info("Initialization of Mozilla Firefox driver.");
 			driver = new FirefoxDriver();
 		} else if (BROWSER.equalsIgnoreCase("ie")) {
+			log.info("Initialization of Internet Explorer driver.");
 			System.setProperty("webdriver.ie.driver", System.getProperty("user.dir") + IEDRIVER_PATH);
 			driver = new InternetExplorerDriver();
 		} else if (BROWSER.equalsIgnoreCase("chrome")) {
+			log.info("Initialization of Google Chrome driver.");
 			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + CHROMEDRIVER_PATH);
 			driver = new ChromeDriver();
 		}
 		driver.manage().window().maximize();
+		
+		log.info("Set values of implicit and explicit waits.");
 		driver.manage().timeouts().implicitlyWait(IMPLICIT_WAIT, TimeUnit.SECONDS);
 		driver.manage().timeouts().pageLoadTimeout(IMPLICIT_WAIT, TimeUnit.SECONDS);
 		wait = new WebDriverWait(driver, EXPLICIT_WAIT, WEBDRIVERWAIT_SLEEP);
@@ -73,9 +82,10 @@ public class SeleniumDriverManager {
 	 */
 	public void quitDriver() {
 		try {
+			log.info("Set null the webdriver.");
 			driver.quit();
 		} catch (Exception e) {
-			Logger.getLogger(getClass()).error("Unable to quit the webdriver", e);
+			log.error("Unable to quit the webdriver", e);
 		}
 		driver = null;
 	}
