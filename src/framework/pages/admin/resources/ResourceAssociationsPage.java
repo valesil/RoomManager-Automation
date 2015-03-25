ab4d22a431af773b799bd684817dce0a41f99417
Marco Llano <llano.marco@gmail.com>
2015-03-25 15:39:02 -0400

    Method clickCloseBtn added

--- src/framework/pages/admin/resources/ResourceAssociationsPage.java
+++ src/framework/pages/admin/resources/ResourceAssociationsPage.java
@@ -1,11 +1,16 @@
 package framework.pages.admin.resources;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import framework.common.UIMethods;
 import framework.selenium.SeleniumDriverManager;
 
 public class ResourceAssociationsPage extends ResourceBaseAbstractPage {
+	@FindBy(xpath = "//span[contains(text(),'Close')and@class='ng-binding']") 
+	WebElement closeBtn;
 	
 	public ResourceAssociationsPage() {		
 		driver = SeleniumDriverManager.getManager().getDriver();
@@ -31,5 +36,13 @@ public class ResourceAssociationsPage extends ResourceBaseAbstractPage {
 			String roomDisplayName) {
 		By name = By.xpath("//span[contains(text(),'" + roomDisplayName + "')]");
 		return UIMethods.isElementPresent(name);
+	}
+	
+	/**
+	 * [ML]Method to click on close button
+	 */
+	public void clickCloseBtn() {
+		wait.until(ExpectedConditions.elementToBeClickable(closeBtn));
+		closeBtn.click();
 	}
 }
